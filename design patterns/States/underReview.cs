b98cf41624baa945c3f77aa28a1e484781765085
גרינצייג חנה שרה <  @mbjcomp.local>
2024-03-31 20:09:53 +0300

    fileSystem

---
+++ design patterns/States/underReview.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns.States
+{
+    internal class underReview
+    {
+    }
+}
