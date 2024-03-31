f7015be50e3683c6ae247989616d8f1720c54a13
sarinailinger <hand1c23@mbjcomp.org.il>
2024-03-31 19:24:51 +0300

    first commit

---
+++ design patterns/Branch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_patterns
+{
+    public class Branch
+    {
+
+    }
+}
